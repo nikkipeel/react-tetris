34ba5d13de4849e87dc977f692105c6383da587d
Nikki <npeel3497@gmail.com>
2020-03-18 17:58:51 -0400

    Updates

---
+++ precache-manifest.4ad21ea9e9c7d3210e2f1dd9c52d0c27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f833b6dd9fc139adffbe68ae4910d00",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "c9cc7300b557ea930312",
+    "url": "/react-tetris/static/css/main.ecc5ab23.chunk.css"
+  },
+  {
+    "revision": "4ce011d8437ea73bcbd2",
+    "url": "/react-tetris/static/js/2.6bfae62b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-tetris/static/js/2.6bfae62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9cc7300b557ea930312",
+    "url": "/react-tetris/static/js/main.a64af58c.chunk.js"
+  },
+  {
+    "revision": "50e76ecefbecfd7c8030",
+    "url": "/react-tetris/static/js/runtime-main.c2e66880.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
